e3a3a1c0c3654ecbc341a3752bd3d5def05c7eb5
Samuel Berthe <dev@samuel-berthe.fr>
2025-09-10 20:29:46 +0200

    fix: fix out of bounds slice

--- dump.go
+++ dump.go
@@ -12,9 +12,11 @@ type bodyWriter struct {
 
 // implements io.Writer
 func (w *bodyWriter) Write(b []byte) (int, error) {
-	if w.body.Len()+len(b) > w.maxSize {
-		w.body.Truncate(min(w.maxSize, len(b), w.body.Len()))
-		return w.body.Write(b[:w.maxSize-w.body.Len()])
+	length := len(b)
+
+	if w.body.Len()+length > w.maxSize {
+		w.body.Truncate(min(w.maxSize, length, w.body.Len()))
+		return w.body.Write(b[:min(w.maxSize-w.body.Len(), length)])
 	}
 	return w.body.Write(b)
 }
@@ -38,7 +40,7 @@ func (r *bodyReader) Read(b []byte) (int, error) {
 	n, err := r.ReadCloser.Read(b)
 	if r.body != nil && r.body.Len() < r.maxSize {
 		if r.body.Len()+n > r.maxSize {
-			r.body.Write(b[:r.maxSize-r.body.Len()])
+			r.body.Write(b[:min(r.maxSize-r.body.Len(), n)])
 		} else {
 			r.body.Write(b[:n])
 		}
